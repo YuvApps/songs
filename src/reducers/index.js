6f3324a39844c4f1efa984c900db2495d6c4d6e6
amir <abachar1210@gmail.com>
2020-03-18 17:08:20 +0200

    new song

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -6,7 +6,8 @@ const SongReducer = () => {
         { title: "Bye Bye", duration: "4:01"},
         { title: "Hey Jude", duration: "2:01"},
         { title: "Mama Mia", duration: "5:01"},
-        { title: "Yeah!", duration: "6:01"}
+        { title: "Yeah!", duration: "6:01"},
+        { title: "fuck!", duration: "2:01"}
     ]
 }
 
